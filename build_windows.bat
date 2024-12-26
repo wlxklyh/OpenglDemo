12c96fe8801569936707e0cd521f6b7ece42f809
wlxklyh <linyanhou@qq.com>
2024-12-27 01:43:39 +0800

    Init

---
+++ build_windows.bat
@@ -0,0 +1,19 @@
+
+@echo off
+setlocal
+
+set dir_name=build
+if exist %dir_name% (
+    echo delete %dir_name%
+    rd /s/q %dir_name%
+)
+md %dir_name%
+echo create %dir_name%
+cd %dir_name%
+
+
+cmake ..
+
+pause 
+
+cmake --build .
